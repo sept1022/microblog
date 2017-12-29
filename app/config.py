63a54957dcd5c044d9f3ea9d97b5fb3f2d267d7f
kyuyeol.han <kyuyeol.han@sk.com>
2017-12-29 13:37:36 +0900

    chapter 11

--- app/config.py
+++ app/config.py
@@ -16,4 +16,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['sept1022@gmail.com']
 
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
--- app/routes.py
+++ app/routes.py
@@ -86,7 +86,8 @@ def user(username):
         if posts.has_next else None
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=['GET','POST'])
