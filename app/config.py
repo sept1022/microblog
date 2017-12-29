81b5c8180de1848b5b7e02194ce881a8f6869ac9
kyuyeol.han <kyuyeol.han@sk.com>
2017-12-29 10:20:10 +0900

    chapter 9

--- app/config.py
+++ app/config.py
@@ -15,3 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['sept1022@gmail.com']
+
+    POSTS_PER_PAGE = 25
